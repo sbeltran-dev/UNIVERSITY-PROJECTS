19b45ed7a74d254d8c77c255da0c4abda0089ab5
Santiago <sbeltranastorga@gmail.com>
2025-10-31 09:32:50 -0700

    Delete 1st Semester directory

--- 1st Semester/Computer Fundamentals I/Unit 1/C/For/Serie_Gregory.c
+++
@@ -1,9 +0,0 @@
-/*
- Calcule el valor de pi a partir de la siguiente serie infinita (Serie Gregory-Leibniz)
- pi = 4/1 - 4/3 + 4/5 - 4/7 + 4/9 - 4/11 +....
- Para 20 iteraciones e imprimir el resultado.
-*/
-#include<stdio.h>
-int main(){
-    double n=4,d=1,pi=0;printf("\n|| SERIE DE GREGORY ||\n");for(int i=1;i<21;i++){pi+=n/d;printf("%.0f/%.0f ",n,d);d++;d++;n=-n;}printf("\nEl valor de Pi es: %f",pi);return 0;
-}
